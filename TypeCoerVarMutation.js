d66b2a1cde1a7034a4bba07b3ccedc310e305748
Dhananjay Jakhadi <djakhad1@binghamton.edu>
2019-06-25 22:54:40 -0400

    Type Coercion and Variable Muatation

--- TypeCoerVarMutation.js
+++ TypeCoerVarMutation.js
@@ -2,9 +2,20 @@
 
 let firstName = "John";
 let age = 27;
+
+//Type Coercion
 console.log(firstName + " " + age);
 
 let job, isMarried;
 job = "Software Engineer";
 isMarried = false;
 console.log( firstName +" is a" +age + " years old. He is a " + job + ". Is he Married ? " + isMarried);
+
+
+//Variable Mutation
+age = 'twenty eight';
+job = 'driver';
+alert( firstName +' is a' +age + ' years old. He is a ' + job + '. Is he Married ? '+ isMarried);
+
+var lastName = prompt('What is his lastName?')
+console.log(firstName+ ' '+lastName);
