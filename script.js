57ca8823f791f877da4525ee1d334b3096468c7c
Dhananjay Jakhadi <djakhad1@binghamton.edu>
2019-06-25 22:49:10 -0400

    Type Coercion and variable Mutation

---
+++ script.js
@@ -0,0 +1,10 @@
+/**Type Coercion and Variable Mutation */
+
+let firstName = "John";
+let age = 27;
+console.log(firstName + " " + age);
+
+let job, isMarried;
+job = "Software Engineer";
+isMarried = false;
+console.log( firstName +" is a" +age + " years old. He is a " + job + ". Is he Married ? " + isMarried);
